536bf641494e46aedc449ada53f65bba1df745e7
debanshusarkar789-hub <debanshusarkar789@gmail.com>
2025-12-14 09:58:07 +0530

    Create buy.js

---
+++ pages/buy.js
@@ -0,0 +1,11 @@
+export default function Buy() {
+  return (
+    <div style={{ padding: 30 }}>
+      <h2>Buy Book Access</h2>
+      <input
+        placeholder="Enter Access Key"
+        style={{ padding: 10, width: 300 }}
+      />
+    </div>
+  )
+}
