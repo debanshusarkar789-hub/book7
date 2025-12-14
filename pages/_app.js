ca7d366b61d85881f1322b534571e3950a9bc5c6
debanshusarkar789-hub <debanshusarkar789@gmail.com>
2025-12-14 09:55:20 +0530

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    fetch('/api/track', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ page: router.pathname })
+    })
+  }, [router.pathname])
+
+  return <Component {...pageProps} />
+}
